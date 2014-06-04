cd4fd44c73edf2ffd11a8db69601e672959e4a31
Mario Perez <mapreal19@gmail.com>
2014-06-04 17:57:00 +0200

    loading state removed

--- app/assets/javascripts/contacto.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function(){
-	$('#submit_email').click(function () {
-		var btn = $(this);
-		btn.button('loading');
-	});
-});
--- app/controllers/incoming_mails_controller.rb
+++ app/controllers/incoming_mails_controller.rb
@@ -2,7 +2,7 @@ class IncomingMailsController < ApplicationController
   def create
   	# name, email, tlf, subject
   	
-  	redirect_to contacto_path, :notice => 'Mensaje'
+  	redirect_to contacto_path, :notice => 'Mensaje enviado'
   	
   end
 end
